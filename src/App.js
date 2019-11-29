cee7b7fdabe3d7910d6f0a6359850043710b21a8
hajczek <i.hajczewska@gmail.com>
2019-11-29 10:55:20 +0100

    Added structure of app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+
+      <div class="container">
+        <div id="display">Result: 0</div>
+        <div id="clear">AC</div>
+        <div id="add">+</div>
+        <div id="subtract">-</div>
+        <div id="multiply">*</div>
+        <div id="divide">/</div>
+        <div id="decimal">.</div>
+        <div id="zero">0</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+        <div id='equals'>=</div>
+      </div>
+
     </div>
   );
 }
