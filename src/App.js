9d25cfd475f5c41e20002fbcb650ad49ce744ac8
hajczek <i.hajczewska@gmail.com>
2019-12-03 09:00:27 +0100

    Changed order of element

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ function App() {
         <div id="subtract" class="small">-</div>
         <div id="multiply" class="small">*</div>
         <div id="divide" class="small">/</div>
-        <div id="decimal" class="small">.</div>
-        <div id="zero" class="small">0</div>
-        <div id="one" class="small">1</div>
-        <div id="two" class="small">2</div>
-        <div id="three" class="small">3</div>
-        <div id="four" class="small">4</div>
-        <div id="five" class="small">5</div>
-        <div id="six" class="small">6</div>
         <div id="seven" class="small">7</div>
         <div id="eight" class="small">8</div>
         <div id="nine" class="small">9</div>
+        <div id="four" class="small">4</div>
+        <div id="five" class="small">5</div>
+        <div id="six" class="small">6</div>
+        <div id="one" class="small">1</div>
+        <div id="two" class="small">2</div>
+        <div id="three" class="small">3</div>
+        <div id="zero" class="small">0</div>
+        <div id="decimal" class="small">.</div>
         <div id='equals' class="small">=</div>
       </div>
 
