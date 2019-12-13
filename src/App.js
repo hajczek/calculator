cbcf35e9f21ecd4c0c888a97e9c16fc2f8618c2f
hajczek <i.hajczewska@gmail.com>
2019-12-13 09:06:46 +0100

    Added functionality for display all number in display section

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ class App extends Component {
     e.preventDefault();
 
     let value = e.target.firstChild.nodeValue;
-
     let allAction = document.getElementById('allAction');
     let finishResult = document.getElementById('finishResult');
 
     if (e.target.nodeName !== 'FORM') {
-      // let display = document.getElementById('display');
 
       allAction.classList.remove('before');
       allAction.classList.add('clearBefore');
@@ -24,7 +22,14 @@ class App extends Component {
       finishResult.classList.add('clearBefore');
 
       allAction.innerHTML += value;
-      finishResult.innerHTML = value;
+      // finishResult.innerHTML = value;
+
+      // If statement for buttons with numbers and decimal point - for displays these together as one element
+      if (e.target.className.includes('num') || e.target.firstChild.nodeValue === '.') {
+        finishResult.innerHTML += value;
+      } else {
+        finishResult.innerHTML = value;
+      };
 
       let result = allAction.firstChild.nodeValue;
       let actual = finishResult.firstChild.nodeValue;
@@ -51,8 +56,6 @@ class App extends Component {
     if (e.target.firstChild.nodeValue === '=') {
       var Parser = require('expr-eval').Parser;
       let score = Parser.evaluate(this.state.result);
-      // console.log(typeof allAction.firstChild.nodeValue);
-      // console.log(score);
 
       // Condition for except with 0.1+0.2 in JS
       if (allAction.firstChild.nodeValue === '0.1+0.2=' ||
@@ -60,14 +63,13 @@ class App extends Component {
         allAction.firstChild.nodeValue === '.2+.1=') {
         score = 0.3
       }
+
       this.setState({
-        result: this.state.result + '=' + score,
+        // result: this.state.result + '=' + score,
+        result: score,
         actual: score
       });
     }
-
-
-
 
     e.stopPropagation();
   }
@@ -87,16 +89,16 @@ class App extends Component {
             <button id="subtract" className="small display act">-</button>
             <button id="multiply" className="small display act">*</button>
             <button id="divide" className="small display act">/</button>
-            <button id="seven" className="small display">7</button>
-            <button id="eight" className="small display">8</button>
-            <button id="nine" className="small display">9</button>
-            <button id="four" className="small display">4</button>
-            <button id="five" className="small display">5</button>
-            <button id="six" className="small display">6</button>
-            <button id="one" className="small display">1</button>
-            <button id="two" className="small display">2</button>
-            <button id="three" className="small display">3</button>
-            <button id="zero" className="small display">0</button>
+            <button id="seven" className="small display num">7</button>
+            <button id="eight" className="small display num">8</button>
+            <button id="nine" className="small display num">9</button>
+            <button id="four" className="small display num">4</button>
+            <button id="five" className="small display num">5</button>
+            <button id="six" className="small display num">6</button>
+            <button id="one" className="small display num">1</button>
+            <button id="two" className="small display num">2</button>
+            <button id="three" className="small display num">3</button>
+            <button id="zero" className="small display num">0</button>
             <button id="decimal" className="small display act">.</button>
             <button id='equals' className="small display equal">=</button>
           </form>
