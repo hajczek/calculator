2e554ca443f6ef60ef7cd5eb42d91bcc7f70dded
hajczek <i.hajczewska@gmail.com>
2019-12-20 17:59:23 +0100

    Updates

---
+++ precache-manifest.20e56b5c05e189f26c3ac76ae67a620e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921efc4fece5be509847fbc42fe4dfeb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ee29a7e242ccdd000609",
+    "url": "/calculator/static/css/main.421b50af.chunk.css"
+  },
+  {
+    "revision": "5b7758a262f7355e4d56",
+    "url": "/calculator/static/js/2.78a9cad0.chunk.js"
+  },
+  {
+    "revision": "ee29a7e242ccdd000609",
+    "url": "/calculator/static/js/main.57bd05b6.chunk.js"
+  },
+  {
+    "revision": "f69b5c5b50f5b7cd07bd",
+    "url": "/calculator/static/js/runtime-main.736c2c55.js"
+  }
+]);
